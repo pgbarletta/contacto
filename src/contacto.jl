e8911150a08be860c724be66149a95f11550a2ba
pbarletta <pbarletta@gmail.com>
2018-09-25 13:44:11 -0300

    I think I'm done with algo

--- src/contacto.jl
+++ src/contacto.jl
@@ -32,62 +32,156 @@ function Voxel(c::Array{Float64, 1}, hd::Float64, level::Int64)
     Voxel(c, vertices, hd, level, Voxel(), MVector{8, Voxel}(fill(Voxel(), 8)))
 end
 
-mutable struct Cell
-    atoms::Array{Int64, 1}
-    vertices::Array{Float64, 2}
+mutable struct node
+    children::Array{node, 1}
+    n::Int64
+    node_level::Int64
+    idx_range::UnitRange{Int64}
+    srt_idx::Array{Int64, 1}
+    natoms::Int64
+    xyz::Array{Float64, 2}
+    
+    node() = new()
 
-    Cell() = new()
-
-    function Cell(atoms::Array{Int64, 1}, vertices::Array{Float64, 2})
-        if size(vertices) != (8, 3)
-            error("Wrong input box. Should be a 8x3 Array{Float64, 2}.")
-        end
-        new(atoms, vertices)
+    function node(ncells::Int64, node_level::Int64)
+        new(Array{node, 1}(undef, ncells), ncells, node_level, 0:0)
     end
-    function Cell(vertices::Array{Float64, 2})
-        if size(vertices) != (8, 3)
-            error("Wrong input box. Should be a 8x3 Array{Float64, 2}.")
-        end
-        new(vertices)
+
+    function node(ncells::Int64, node_level::Int64)
+        new(Array{node, 1}(undef, ncells), ncells, node_level)
+    end
+
+    function node(ncells::Int64, node_level::Int64, idx_range::UnitRange{Int64},
+        srt_idx::Array{Int64, 1})
+        new(Array{node, 1}(undef, ncells), ncells, node_level, idx_range, srt_idx,
+        length(srt_idx))
+    end
+
+    function node(ncells::Int64, node_level::Int64, idx_range::UnitRange{Int64},
+        srt_idx::Array{Int64, 1}, xyz::Array{Float64, 2})
+        new(Array{node, 1}(undef, ncells), ncells, node_level, idx_range, srt_idx,
+            length(srt_idx), xyz)
     end
 end
 
-mutable struct Cell_list
-    cells::Array{Cell, 3}
+function Base.getindex(X::node, i::Int)
+    i > X.n ? throw(BoundsError(X.n, i)) : return X.children[i]
+end
+
+function Base.setindex!(X::node, v::node, i::Int)
+    i > X.n ? throw(BoundsError(X.n, i)) : X.children[i] = v
+end
+
+function Base.getindex(X::node, i::Int, j::Int)
+    if i <= X.n & j <= X.n
+        return X[i][j]
+    else
+        throw(BoundsError([X.n X.n], [i, j]))
+    end
+end
+
+function Base.setindex!(X::node, v::node, i::Int, j::Int)
+    if i <= X.n & j <= X.n
+        X.children[i][j] = v
+    else
+        throw(BoundsError([X.n X.n], [i, j]))
+    end
+end
+
+function Base.getindex(X::node, i::Int, j::Int, k::Int)
+    if i <= X.n & j <= X.n & k <= X.n
+        return X[i][j][k]
+    else
+        throw(BoundsError([X.n X.n X.n], [i, j, k]))
+    end
+end
+
+function Base.setindex!(X::node, v::node, i::Int, j::Int, k::Int)
+    if i <= X.n & j <= X.n & k <= X.n
+        X.children[i][j][k] = v
+    else
+        throw(BoundsError([X.n X.n X.n], [i, j, k]))
+    end
+end
+
+Base.first(X::node) = 1
+Base.last(X::node) = length(X.children)
+
+function Base.size(X::node)
+    return length(X.srt_idx)
+end
+
+# I probably shouldn't allow setindex!() as the user will likely
+# invalidate the rest of the tree
+mutable struct tree
+    root::node
+    ncells::Int64
     
-    Cell() = new()
+    tree() = new()
+
+    tree(root, ncells) = new(root, ncells)
     
-    
-    function Cell_list(bbox::Array{Float64, 2})
+    function tree(ncells::Int64, natoms::Int64, xyz::Array{Float64, 2})
+        root = node(ncells, 0, 1:natoms, sortperm(xyz[:, 1]), xyz)
         
-        new(atoms, vertices)
+        for i = 1:ncells
+            root[i] = node(ncells, 1)
+            for j = 1:ncells
+                root[i][j] = node(ncells, 2)
+                for k = 1:ncells
+                    root[i][j][k] = node(0, 3)
+                end
+            end
+        end            
+        new(root, ncells)
+    end
+    
+end
+
+function Base.getindex(X::tree, i::Int)
+    i > X.ncells ? throw(BoundsError(X.ncells, i)) : return X.root.children[i]
+end
+
+function Base.setindex!(X::tree, v::node, i::Int)
+    i > X.ncells ? throw(BoundsError(X.ncells, i)) : X.root.children[i] = v
+end
+
+function Base.getindex(X::tree, i::Int, j::Int)
+    if i <= X.ncells & j <= X.ncells
+        return X.root.children[i][j]
+    else
+        throw(BoundsError([X.ncells X.ncells], [i, j]))
     end
 end
 
-function Cell_list(t_xyz::Array{Float64, 2})
-    
-    new(atoms, vertices)
+function Base.setindex!(X::tree, v::node, i::Int, j::Int)
+    if i <= X.ncells & j <= X.ncells
+        X.root.children[i][j] = v
+    else
+        throw(BoundsError([X.ncells X.ncells], [i, j]))
+    end
 end
-# tmp_xyz = Array{Float64}(undef, natoms, 3)
-# tmp_xyz[:, 1] = t_xyz[:, 1] .- ctro[1]
-# tmp_xyz[:, 2] = t_xyz[:, 2] .- ctro[2]
-# tmp_xyz[:, 3] = t_xyz[:, 3] .- ctro[3];
-# gr_prt = convert(Array{Int64, 2}, round.(tmp_xyz ./ resolucion));;
 
-# ref_point = Int64
-# i = argmax(espacio)
-# if i == 1
-#     ref_point = x_min
-# elseif i == 2
-#     ref_point = y_min
-# elseif i == 3
-#     ref_point = z_min
-# else
-#     error("loquisimo")
-# end
+function Base.getindex(X::tree, i::Int, j::Int, k::Int)
+    if i <= X.ncells & j <= X.ncells & k <= X.ncells
+        return X.root.children[i][j][k]
+    else
+        throw(BoundsError([X.ncells X.ncells X.ncells], [i, j, k]))
+    end
+end
 
-# round((half_dim - (half_dim % resolution)) / resolution)
-# 
-# rsltion = 0.01
+function Base.setindex!(X::tree, v::node, i::Int, j::Int, k::Int)
+    if i <= X.ncells & j <= X.ncells & k <= X.ncells
+        X.root.children[i][j][k] = v
+    else
+        throw(BoundsError([X.ncells X.ncells X.ncells], [i, j, k]))
+    end
+end
+
+Base.first(X::tree) = 1
+
+function Base.size(X::tree)
+    return X.ncells^3
+end
 
 end
